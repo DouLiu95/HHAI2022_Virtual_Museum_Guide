09a4a4d35f92dfbfaada9b2a565d72265a9a56a6
DouLiu <LDLucien@hotmail.com>
2022-05-25 11:21:26 +0200

    ES

---
+++ embedding_es.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
+from scipy.spatial.distance import pdist
+def get_similarity(name,exception_list,label =None):
+    embedding = pd.read_csv("component/export.csv")
+    embedding = embedding[((embedding.label != '[]')&(embedding.embedding!='[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]'))]
+    if name.startswith('Q'):
+        name = r"http://www.wikidata.org/entity/" +name
+    print("these painting is excluded: ",exception_list)
+    if exception_list:
+        for i in exception_list:
+            if i != []:
+                if isinstance(i,list):
+                    for item in i:
+                        if item != name: # in case we exclude the entity the user is searching
+                            embedding = embedding[(embedding.uri != item)]
+                elif isinstance(i,str):
+                    if i != name: # in case we exclude the entity the user is searching
+                        embedding = embedding[(embedding.uri != i)]
+
+
+    embedding['embedding'] = embedding.apply(lambda row : eval(row['embedding']), axis = 1)
+    if label:
+        if label == 'Exhibit':
+            label = 'Paintings'
+        # if label is specify, then we return the items of that kind of label
+        # test = list(embedding[(embedding.name == name)].embedding)
+        # print(embedding[(embedding.name == name)])
+        try:
+            curent_id = int(embedding[(embedding.uri == name)].nodeId)
+            test = list(embedding[(embedding.uri == name)].embedding)
+        except:
+            curent_id = int(embedding[(embedding.uri == name)].iloc[0].nodeId)
+            test = list(embedding[(embedding.uri == name)].iloc[0].embedding)
+        print(curent_id)
+        similarity_score = []
+        for index, row in embedding.iterrows():
+            Vec = np.vstack([test, row.embedding])
+            dist2 = 1 -pdist(Vec, 'cosine')
+            similarity_score.append(dist2)
+        embedding['similarity'] = similarity_score
+        print(embedding[((embedding.label == '['+label+']') & (embedding.nodeId != curent_id) )])
+
+        sorted_df = embedding[((embedding.label == '['+label+']') & (embedding.nodeId != curent_id) )].sort_values(by ="similarity",ascending=False)
+        print(sorted_df)
+
+        return curent_id, sorted_df.iloc[0].nodeId
+    else:
+        # if label is not specify, then we return the items with same label
+        try:
+            curent_id = int(embedding[(embedding.uri == name)].nodeId)
+            test = list(embedding[(embedding.uri == name)].embedding)
+        except:
+            curent_id = int(embedding[(embedding.uri == name)].iloc[0].nodeId)
+            test = list(embedding[(embedding.uri == name)].iloc[0].embedding)
+        label = embedding[(embedding.uri == name)].label.item()
+        print('label is', label)
+        similarity_score = []
+        for index, row in embedding.iterrows():
+            Vec = np.vstack([test, row.embedding])
+            dist2 = 1 - pdist(Vec, 'cosine')
+            similarity_score.append(dist2)
+        embedding['similarity'] = similarity_score
+        sorted_df = embedding[((embedding.label == label)&(embedding.nodeId != curent_id))].sort_values("similarity",ascending=False)
+        print(sorted_df)
+        return curent_id,sorted_df.iloc[0].nodeId
+
+# print(get_similarity(' Hendrik Heerschop',label='Exhibit'))
---
+++ graph_database_es.py
@@ -0,0 +1,393 @@
+from py2neo import Node, Relationship, Graph
+from component.question_classfier import *
+from component.question_parser_es import *
+import random
+import pandas as pd
+class ArtGraph:
+    def __init__(self):
+        self.g = Graph(
+            scheme="neo4j",
+            host="localhost",
+            port=7687,
+            auth=("neo4j", "000000"))
+        self.node_name_list = ['Paintings','Keyword','Country','Genre','Material','Collection','Person','Exhibition','Movement','City']
+        self.Paintings = []
+        self.Keyword = []
+        self.Country = []
+        self.Genre = []
+        self.Material = []
+        self.Collection = []
+        self.Person = []
+        self.Exhibition = []
+        self.Movement  = []
+        self.City = []
+        self.num_limit = 20
+        self.classifier = QuestionClassifier()
+        self.parser = QuestionPaser()
+        with open('dict/Paintings.txt', encoding="utf-8") as f:
+            self.Paintings = [i.strip() for i in f.readlines()]
+
+        with open('dict/Keyword.txt', encoding="utf-8") as f:
+            self.Keyword = [i.strip() for i in f.readlines()]
+
+        with open('dict/Country.txt', encoding="utf-8") as f:
+            self.Country = [i.strip() for i in f.readlines()]
+
+        with open('dict/Genre.txt', encoding="utf-8") as f:
+            self.Genre = [i.strip() for i in f.readlines()]
+
+        with open('dict/Material.txt', encoding="utf-8") as f:
+            self.Material = [i.strip() for i in f.readlines()]
+
+        with open('dict/Collection.txt', encoding="utf-8") as f:
+            self.Collection = [i.strip() for i in f.readlines()]
+
+
+
+        with open('dict/Person.txt', encoding="utf-8") as f:
+            self.Person = [i.strip() for i in f.readlines()]
+
+        with open('dict/Exhibition.txt', encoding="utf-8") as f:
+            self.Exhibition = [i.strip() for i in f.readlines()]
+
+        with open('dict/Movement.txt', encoding="utf-8") as f:
+            self.Movement = [i.strip() for i in f.readlines()]
+
+        with open('dict/City.txt', encoding="utf-8") as f:
+            self.City = [i.strip() for i in f.readlines()]
+        # print(self.Paintings)
+
+        self.exhibit_checked = []
+        self.exhibit_unchecked = ['Two moors', 'King Caspar', 'Don Miguel de Castro, Emissary of Congo',
+                                  'Diego Bemba, a Servant of Don Miguel de Castro',
+                                  'Pedro Sunda, a Servant of Don Miguel de Castro',
+                                  'Map of Paranambucae', 'Portrait of a Black Girl', 'Portrait of a Man',
+                                  'Man in a Turban',
+                                  'Head of a Boy in a Turban', 'Doritos',
+                                  'The New Utopia Begins Here: Hermina Huiswoud',
+                                  'The Unspoken Truth', 'Ilona', 'Head of a Boy', 'The Market in Dam Square']
+
+        self.preference = {'Paintings': 1,
+                           'Person': 1,
+                           'Collection': 1,
+                           'Exhibition': 1,
+                           'Genre': 1,
+                           'Keyword': 1,
+                           'Movement': 1,
+                           'Material': 1,
+                           'Exhibit': len(self.exhibit_unchecked)}
+
+        self.history = pd.DataFrame(columns = [ 'session',
+                                    'intent',
+                                    'queryText',
+                                    'parameters',
+                                                'parameters_dic','item_preference',
+                                                'response.entity','exhibit_checked','exhibit_unchecked','preference','Cypher'])
+
+        self.preference_list = ['Paintings','Keyword','Genre','Material','Collection','Person','Exhibition','Movement']
+
+    def build_nodes(self):
+        # kinds of nodes
+        Paintings = []
+        Keyword = []
+        Country = []
+        Genre = []
+        Material = []
+        Collection = []
+        Person = []
+        Exhibition = []
+        Movement = []
+        City = []
+        nodes_list=[Paintings,Keyword,Country,Genre,Material,Collection,Person,Exhibition,Movement,City]
+        # kinds of relations
+        in_collection = [] #collection-[]-paintings
+        keyword = [] # paintings-[]-depicts
+        has_genre = [] # paintings - [] - genre
+        has_material = [] # paintings - [] - material
+        country = [] # paintings - [] - country
+
+        for index,nodes in enumerate(self.node_name_list):
+            print(nodes)
+            sparql_for_name = "MATCH (n:"+str(nodes)+") RETURN n"
+            nodes_data_all = self.g.run(str(sparql_for_name)).data()
+            for node in nodes_data_all:
+                try:
+                    nodes_list[index].append(node['n']['name'])
+
+                except :
+                    pass
+
+            # if nodes == 'Paintings':
+            #     with open('./dict/' + str('title') + '.txt', 'w+', encoding="utf-8") as f:
+            #         f.write('\n'.join(list(title)))
+        # write the txt of data
+            with open('./dict/'+str(nodes)+'.txt', 'w+',encoding="utf-8") as f:
+                f.write('\n'.join(list(nodes_list[index])))
+
+
+    def show_exhibit(self,exhibit_in_dialog):
+        if exhibit_in_dialog in self.Paintings:
+            if '_' not in exhibit_in_dialog:
+                exhibit_in_dialog = r"http://www.wikidata.org/entity/" + exhibit_in_dialog
+            sparql_for_name = "MATCH (n:Paintings) WHERE n.uri = '" + str(exhibit_in_dialog) +"' RETURN n"
+            data = self.g.run(str(sparql_for_name)).data()
+            print(data)
+            return data
+        # elif exhibit_in_dialog in self.Paintings_title:
+        #     sparql_for_name = "MATCH (n:Paintings) WHERE n.title = '" + str(exhibit_in_dialog) + "' RETURN n"
+        #     data = self.g.run(str(sparql_for_name)).data()
+        #     print(data)
+        #     return data
+        elif exhibit_in_dialog in self.Person:
+            sparql_for_name = "MATCH (n:Person) WHERE n.uri = '" + str(exhibit_in_dialog) + "' RETURN n"
+            data = self.g.run(str(sparql_for_name)).data()
+            print(data)
+            return data
+        else:
+            return None
+
+    def search_by_nodeId(self, nodeid):
+        sparql_with_nodeid = "MATCH (n) WHERE id(n) = "+str(nodeid)+" RETURN n"
+        data = self.g.run(str(sparql_with_nodeid)).data()
+        print(data)
+        return data
+
+    # def update_exhibit(self,exhibit_name):
+    #     if exhibit_name in self.exhibit_unchecked:
+    #         self.exhibit_unchecked.remove(exhibit_name)
+    #         self.exhibit_checked.append(exhibit_name)
+    #         self.preference['Exhibit'] = len(self.exhibit_unchecked)
+
+    # def update_user_preference(self,item):
+    #
+    #     if item in self.Paintings and self.preference['Paintings'] <=10:
+    #         self.preference['Paintings'] +=1
+    #     if item in self.Collection and self.preference['Collection'] <=10:
+    #         self.preference['Collection'] +=1
+    #     if item in self.Exhibition and self.preference['Exhibition'] <=10:
+    #         self.preference['Exhibition'] +=1
+    #     if item in self.Genre and self.preference['Genre'] <=10:
+    #         self.preference['Genre'] +=1
+    #     if item in self.Keyword and self.preference['Keyword'] <=10:
+    #         self.preference['Keyword'] +=1
+    #     if item in self.Movement and self.preference['Movement'] <=10:
+    #         self.preference['Movement'] +=1
+    #     if item in self.Material and self.preference['Mterial'] <=10:
+    #         self.preference['Mterial'] +=1
+    #     if item in self.Person and self.preference['Person'] <=10:
+    #         self.preference['Person'] +=1
+
+
+    def transfer_request_to_dict(self,request_json, item_preference = '', returned_entity= '',Cypher = None ):
+        '''
+        here to store the interaction between dialogflow and end user, we save part of the information in requests from dialogflow
+        and the returned entity from webhook to the dialogflow as a dataframe and update these information, as well as user's
+        preference.
+        :param request_json: the request sent from dialogflow, json
+        :param returned_entity: the name of returned entity from webhook service, string
+        :param item_preference: the item user checked that should have impact preference, string
+        :return:
+        '''
+        print("=" * 10 + 'item_preference and returned_entity' + "=" * 10)
+        print("item_preference is {}, returned_entity is {}".format(item_preference, returned_entity))
+        d = {}
+        parameters_dic = request_json['queryResult']['parameters']
+
+        key_para = [key for key in parameters_dic.keys() if parameters_dic[key] != '']
+        parameter_list = [parameters_dic[key] for key in parameters_dic.keys() if parameters_dic[key] != '']
+
+        d["session"] = request_json["session"]
+        d['intent'] = request_json['queryResult']['intent']['displayName']
+        d['queryText'] = request_json['queryResult']['queryText']
+        d['item_preference'] = item_preference
+        d['response.entity'] = returned_entity
+        d['parameters'] = parameter_list
+        d['parameters_key'] = key_para
+
+        if Cypher:
+            d['Cypher'] = Cypher
+        else:
+            d['Cypher'] = ''
+        # here check if this session exists in history
+        if d['session'] in self.history['session'].to_list():
+            d['exhibit_checked'] = self.history[self.history['session'].isin([d['session']])].iloc[-1]['exhibit_checked']
+            d['exhibit_unchecked'] = self.history[self.history['session'].isin([d['session']])].iloc[-1]['exhibit_unchecked']
+            d['preference'] = self.history[self.history['session'].isin([d['session']])].iloc[-1]['preference']
+        else:
+            d['exhibit_checked'] = []
+            d['exhibit_unchecked'] = ['Two moors', 'King Caspar', 'Don Miguel de Castro, Emissary of Congo',
+                                  'Diego Bemba, a Servant of Don Miguel de Castro',
+                                  'Pedro Sunda, a Servant of Don Miguel de Castro',
+                                  'Map of Paranambucae', 'Portrait of a Black Girl', 'Portrait of a Man',
+                                  'Man in a Turban',
+                                  'Head of a Boy in a Turban', 'Doritos',
+                                  'The New Utopia Begins Here: Hermina Huiswoud',
+                                  'The Unspoken Truth', 'Ilona', 'Head of a Boy', 'The Market in Dam Square']
+            d['preference'] = {'Paintings': 1,
+                           'Person': 1,
+                           'Collection': 1,
+                           'Exhibition': 1,
+                           'Genre': 1,
+                           'Keyword': 1,
+                           'Movement': 1,
+                           'Material': 1,
+                           'Exhibit': len(d['exhibit_unchecked'])}
+        # update the checked list and preference
+        if (isinstance(item_preference,list)):
+            for item in item_preference:
+                if item in d['exhibit_unchecked']:
+                    d['exhibit_unchecked'].remove(item)
+                    d['exhibit_checked'].append(item)
+                    d['preference']['Exhibit'] = len(d['exhibit_unchecked'])
+
+                if item in self.Paintings and d['preference']['Paintings'] <= 10:
+                    d['preference']['Paintings'] += 1
+                if item in self.Collection and d['preference']['Collection'] <= 10:
+                    d['preference']['Collection'] += 1
+                if item in self.Exhibition and d['preference']['Exhibition'] <= 10:
+                    d['preference']['Exhibition'] += 1
+                if item in self.Genre and d['preference']['Genre'] <= 10:
+                    d['preference']['Genre'] += 1
+                if item in self.Keyword and d['preference']['Keyword'] <= 10:
+                    d['preference']['Keyword'] += 1
+                if item in self.Movement and d['preference']['Movement'] <= 10:
+                    d['preference']['Movement'] += 1
+                if item in self.Material and d['preference']['Mterial'] <= 10:
+                    d['preference']['Mterial'] += 1
+                if item in self.Person and d['preference']['Person'] <= 10:
+                    d['preference']['Person'] += 1
+        elif (isinstance(item_preference,str)):
+
+            if item_preference in d['exhibit_unchecked']:
+                d['exhibit_unchecked'].remove(item_preference)
+                d['exhibit_checked'].append(item_preference)
+                d['preference']['Exhibit'] = len(d['exhibit_unchecked'])
+
+            if item_preference in self.Paintings and d['preference']['Paintings'] <=10:
+                d['preference']['Paintings'] +=1
+            if item_preference in self.Collection and d['preference']['Collection'] <=10:
+                d['preference']['Collection'] +=1
+            if item_preference in self.Exhibition and d['preference']['Exhibition'] <=10:
+                d['preference']['Exhibition'] +=1
+            if item_preference in self.Genre and d['preference']['Genre'] <=10:
+                d['preference']['Genre'] +=1
+            if item_preference in self.Keyword and d['preference']['Keyword'] <=10:
+                d['preference']['Keyword'] +=1
+            if item_preference in self.Movement and d['preference']['Movement'] <=10:
+                d['preference']['Movement'] +=1
+            if item_preference in self.Material and d['preference']['Material'] <=10:
+                d['preference']['Material'] +=1
+            if item_preference in self.Person and d['preference']['Person'] <=10:
+                d['preference']['Person'] +=1
+
+        if isinstance(returned_entity, list):
+            for item in returned_entity:
+                if item in d['exhibit_unchecked']:
+                    d['exhibit_unchecked'].remove(item)
+                    d['exhibit_checked'].append(item)
+                    d['preference']['Exhibit'] = len(d['exhibit_unchecked'])
+        elif isinstance(returned_entity, str):
+            if returned_entity in d['exhibit_unchecked']:
+                d['exhibit_unchecked'].remove(returned_entity)
+                d['exhibit_checked'].append(returned_entity)
+                d['preference']['Exhibit'] = len(d['exhibit_unchecked'])
+        self.history = self.history.append(d,ignore_index=True)
+        print("=" * 10+'history'+"=" * 10)
+        print(d)
+        print(self.history)
+        print("=" * 20)
+        self.history.to_csv("history.csv")
+
+    def decide_label_preference(self,mode,session):
+        '''
+        :mode: suggest the mode
+        based on the user_peference to choose a label to suggest, in suggest_question mode,
+        the question should be based on the current item.
+        :return: return the label choose randomly
+        '''
+        if session in self.history['session'].to_list():
+            preference = self.history[self.history['session'].isin([session])].iloc[-1]['preference']
+        else:
+            print("check decide_label_preference")
+        if mode =='recommendation':
+            target = random.randint(0,sum(preference.values()))
+            sum_ = 0
+            for k, v in preference.items():
+                sum_ += v
+                if sum_ >= target:
+                    return k
+        elif mode == 'suggest_question':
+            target = random.randint(0, sum(preference.values())-preference['Exhibit'])
+            sum_ = 0
+            for k, v in preference.items():
+                if k == 'Exhibit':
+                    pass
+                else:
+                    sum_ += v
+                    if sum_ >= target:
+                        return k
+
+    def answer_prettify(self, question):
+
+        try:
+
+            sqls = self.parser.parser_main(question)
+            if not sqls:
+                return None
+            else:
+
+                answer_neo4j = self.g.run(str(sqls[0]['sql'][0])).data()
+                return answer_neo4j
+        except:
+            return None
+        '''
+        can not find anything related
+        Return None and then either the ask for clarification or say sorry to the user
+        '''
+
+    def get_neighbors(self,name,type='',hop=1):
+        '''
+        Get the neighbor nodes of given node
+        :param name: the name of the given node
+        :param type: the entity type of the given node, could be ignored
+        :param hop: the distance, default = 1
+        :return: list of nodes
+        '''
+        if name.startswith('Q'):
+            name = r"http://www.wikidata.org/entity/" + name
+        if type != '':
+            type = ':'+type
+        cypher_neighbors = "MATCH (p"+type+ "{uri: \""+name+"\"}) CALL apoc.neighbors.athop(p, \"belongsto_GENRE|has_CREATOR|belongsto_MOVEMENT|has_KEYWORD|on_MATERIAL|in_COLLECTION|in_EXHIBITION\", +"+str(hop)+") YIELD node RETURN node"
+        data = self.g.run(str(cypher_neighbors)).data()
+        return data
+
+    def get_properties(self,name,type=""):
+        '''
+
+        :param name:
+        :param type:
+        :return: a list of properties's name
+        '''
+        if type != '':
+            type = ':'+type
+        cypher_properties = "Match (p"+type+"{name:\'"+name+"\'}) return keys(p)"
+        data = self.g.run(str(cypher_properties)).data()
+        return data
+
+
+    # def get_neighbors(self,name):
+
+
+if __name__ == '__main__':
+    handler = ArtGraph()
+    # handler.build_nodes()
+    # handler.create_graphrels()
+    # handler.export_data()
+    data = handler.show_exhibit('Rembrandt')
+    print(data)
+    print('Done')
+    data1 = handler.g.run("MATCH (a),  (a)-[]-(b{name:'portrait'}) RETURN a limit 1").data()
+    print(data1[0]['a'])
+# graph.schema.node_labels
+
+# graph.schema.relationship_types
